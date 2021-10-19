8855415981f7f55c345a1f6cc11d7dc842075973
ajknh <91566761+ajknh@users.noreply.github.com>
2021-10-19 15:22:15 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-## main game file
+## main py game file
+import pygame
+pygame.init()
+from pygame.locals import (
+    K_UP,
+    K_DOWN,
+    K_LEFT,
+    K_RIGHT,
+    K_ESCAPE,
+    KEYDOWN,
+    QUIT,
+)
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
