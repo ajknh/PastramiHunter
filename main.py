968237e1a5830c44a4fbecd7d3343aa926da6c44
ajknh <91566761+ajknh@users.noreply.github.com>
2021-10-28 13:34:27 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,11 @@
 import pygame
 import random
 
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+
 gameDisplay = pygame.display.set_mode((800, 600))
 pygame.display.set_caption('Pastrami Hunter')
 exitgame = False
@@ -10,6 +15,7 @@ while not exitgame:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             exitgame = True
-    pygame.display.update()
+    gameDisplay.fill(BLACK)
+    pygame.display.flip()
 pygame.quit()
 quit()
